f9564c2db8ce1f27e913dd344102314a4e055ea8
shivam-sharma0 <vashistshivam931@gmail.com>
2023-07-04 12:02:46 +0530

    Fifth Try

--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -25,10 +25,10 @@ public class WebSeriesService {
         //use function written in Repository Layer for the same
         //Don't forget to save the production and webseries Repo
 
-//       WebSeries webSeries= webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName());
-//        if (webSeries==null){
-//            throw new Exception("Series is already present");
-//        }
+       WebSeries webSeries= webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName());
+        if (webSeries==null){
+            throw new Exception("Series is already present");
+        }
 
         ProductionHouse productionHouse=productionHouseRepository.findById(webSeriesEntryDto.getProductionHouseId()).get();
 
