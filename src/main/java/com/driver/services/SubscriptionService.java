6ca044156db51e5f73a1fd97f1ef22e2a76f66e8
shivam-sharma0 <vashistshivam931@gmail.com>
2023-07-04 12:19:54 +0530

    Seventh  Try

--- src/main/java/com/driver/services/SubscriptionService.java
+++ src/main/java/com/driver/services/SubscriptionService.java
@@ -64,16 +64,39 @@ public class SubscriptionService {
         //If you are already at an ElITE subscription : then throw Exception ("Already the best Subscription")
         //In all other cases just try to upgrade the subscription and tell the difference of price that user has to pay
         //update the subscription in the repository
+        User user=userRepository.findById(userId).get();
+        if(user.getSubscription().equals(SubscriptionType.ELITE)){
+            throw new Exception("Already the best Subscription");
+        }
 
-        return null;
+        Subscription subscription=user.getSubscription();
+        Integer previousFair=subscription.getTotalAmountPaid();
+
+        if(subscription.getSubscriptionType().equals(SubscriptionType.BASIC)){
+            subscription.setSubscriptionType(SubscriptionType.PRO);
+            Integer currentFair=previousFair+300+(50*subscription.getNoOfScreensSubscribed());
+            subscription.setTotalAmountPaid(currentFair);
+        }else {
+            subscription.setSubscriptionType(SubscriptionType.ELITE);
+            Integer currentFair=previousFair+200+(100*subscription.getNoOfScreensSubscribed());
+            subscription.setTotalAmountPaid(currentFair);
+        }
+        user.setSubscription(subscription);
+        subscriptionRepository.save(subscription);
+        return subscription.getTotalAmountPaid()-previousFair;
     }
 
     public Integer calculateTotalRevenueOfHotstar(){
 
         //We need to find out total Revenue of hotstar : from all the subscriptions combined
         //Hint is to use findAll function from the SubscriptionDb
+        List<Subscription> subscriptionList=subscriptionRepository.findAll();
+        Integer totalRevenue=0;
 
-        return null;
+        for(Subscription subscription:subscriptionList){
+            totalRevenue+=subscription.getTotalAmountPaid();
+        }
+        return totalRevenue;
     }
 
 }
